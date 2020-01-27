dbbf4c3923041f10635e4c693bd321eebccf147e
natsuki525 <eteespoir@icloud.com>
2020-01-27 02:29:32 +0000

    [add]relationモデル・コントローラ作成/ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
   devise_scope :user do
       root :to => 'home#top'
   end
   get 'home/about' => 'home#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books do
   	resource :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]

2ca5b24f7a5b86de05703d9d36f1392c1891bc88
natsuki525 <eteespoir@icloud.com>
2020-01-27 02:44:47 +0000

    [add] アソシエーション記述

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,7 @@
 class Relationship < ApplicationRecord
+	# :class_name - 関連するモデルクラス名を指定。関連名と参照先のクラス名を分けたい場合に使う。
+	# belongs_to 変更したい親モデル名, class_name: "元々の親モデル名"
+	belongs_to :following, class_name: "User"
+	belongs_to :follower, class_name: "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,23 @@ class User < ApplicationRecord
   has_many :books
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  # :foreign_key - 参照先のテーブルの外部キーのカラム名を指定できる
+  # -------------自分がフォローしているユーザーとの関連-------------------
+  has_many :active_relationships, class_name: "Rerationship", :foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  # -----------------------------------------------------------------
+
+  # ------------自分がフォローされるユーザーとの関連----------------------
+  has_many :passive_relationships, class_name: "Rerationship", :foreign_key: :follower_id
+  has_many :followes, through: :passive_relationships, source: :following
+  # ------------------------------------------------------------------
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
+
+
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。

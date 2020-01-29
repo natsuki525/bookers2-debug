ec2113a4814112a2c670639f999c325fe8ee68bd
natsuki525 <eteespoir@icloud.com>
2020-01-29 04:48:48 +0000

    [add] jobを編集

--- app/jobs/chat_broadcast_job.rb
+++ app/jobs/chat_broadcast_job.rb
@@ -1,7 +1,13 @@
 class ChatBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(chat)
+  	ActionCable.server.broadcast "room_channel_#{chat.room_id}", chat: render_chat(chat)
     # Do something later
   end
+
+  private
+  	def render_chat(chat)
+  		AppricationController.render.render partial: 'chat/chat',locals: {chat: chat}
+  	end
 end

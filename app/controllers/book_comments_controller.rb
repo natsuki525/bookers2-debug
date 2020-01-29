854468a60965d959f7242288ccae2bc6c9a6c24a
natsuki525 <eteespoir@icloud.com>
2020-01-29 02:44:45 +0000

    [fix] コメント機能非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,19 @@
 class BookCommentsController < ApplicationController
 def create
-	book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_to book_path(book)
+	@book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+    	# redirect_back(fallback_location: book_path(book))
+    end
 end
 
 def destroy
-	book = Book.find(params[:book_id])
-	book_comment = book.book_comments.find(params[:id])
-	book_comment.destroy
-	redirect_back(fallback_location: book_path(book))
+	@book = Book.find(params[:book_id])
+	@book_comment = @book.book_comments.find(params[:id])
+	if @book_comment.destroy
+		# redirect_back(fallback_location: book_path(book))
+	end
 end
 
 private

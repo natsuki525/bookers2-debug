2eee4251eecf908cacf4f3bc9e1267ccb3e7f26f
natsuki525 <eteespoir@icloud.com>
2020-01-29 04:29:24 +0000

    [fix] 記述修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,7 @@ class RoomsController < ApplicationController
   	@room = Room.find(params[:id])
     #present?の戻り値は真偽値。よって、trueの場合、
     if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
-      @direct_messages = @room.direct_messages
+      @chats = @room.chats
       @user_rooms = @room.user_rooms
     else
       redirect_back(fallback_location: root_path)
